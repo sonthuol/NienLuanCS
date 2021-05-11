d468ce0c8faca5a80e86da37066a7fea8a7e0997
Mark Son <63505302+sonthuol@users.noreply.github.com>
2021-05-12 01:20:07 +0700

    Cap nhat lai so luong san pham sau khi thanh toan

--- admin/thanhtoan.php
+++ admin/thanhtoan.php
@@ -20,15 +20,6 @@
             include 'menu_trai.php';
         ?>
         <div id="noidungthanhtoan">
-            <div id="from_thongtinkhachhang">
-            <h2>Thanh toán</h2>
-                <form action="">
-                    <label for="">Tên KH:</label>
-                    <input type="text" name="tenkhachhang" placeholder="Nhập tên khách hàng">
-                    <label for="">SĐT:</label>
-                    <input type="text" name="số điện thoại" placeholder="Nhập số điện thoại">
-                </form>
-            </div>
             <div id="thanhtoan">
                 <div id="sanphamdachon">
                     <h4>Sản phẩm khách hàng mua</h4>
@@ -41,11 +32,17 @@
                                 <td>số lượng</td>
                                 <td>Đơn giá</td>
                                 <td>Thành tiền</td>
-                                <td>Xoá</td>
+                                <td>Xoá </td>
                             </tr>
                         </table>
                         <div id="thanhtoandonhang">
-                            <input type="submit" value=" Thanh toán">
+                            <form action="thanhtoan_hoadon.php" method="POST">
+                                <label for="">Tên KH:</label>
+                                <input type="text" name="tenkhachhang" placeholder="Nhập tên khách hàng">
+                                <label for="">SĐT:</label>
+                                <input type="text" name="sodienthoai" placeholder="Nhập số điện thoại">
+                                <input type="submit" value="Thanh toán">
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -75,15 +72,36 @@
             xmlhttp.send();
         }
         
-        function chonsanpham_thanhtoan(idsp){            
-            var tensp = document.getElementById('tensp_'+idsp).innerHTML;
-            var giaban = document.getElementById('gia_sp_'+idsp).innerHTML;
-            var bang = document.getElementById("sanphamkhchon").innerHTML;
-            document.getElementById('sanphamkhchon').innerHTML = bang + "<tr id="+idsp+">"+"<td>1</td>"+"<td>"+idsp+"</td>"+"<td>"+tensp+"</td>"+"<td>1</td>"+"<td>"+giaban+"</td>"+"<td>"+giaban+"</td>"+"<td><button onclick='xoasanpham_thanhtoan("+idsp+")'><img src='../img/delete.png' alt='' width='20px' height='20px'></button></td>"+"</tr>";
+        function chonsanpham_thanhtoan(idsp){
+            if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            } else { // code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            xmlhttp.onreadystatechange=function() {
+                if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                    document.getElementById("sanphamkhchon").innerHTML=xmlhttp.responseText; 
+                } 
+            }   
+            xmlhttp.open("GET", "../ajax/chonsp.php?idsp="+idsp, true);
+            xmlhttp.send();
         }
 
         function xoasanpham_thanhtoan(idsp){
-            var idsp = document.getElementById(idsp).style.display = 'none';
+            if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp=new XMLHttpRequest();
+            } else { // code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            xmlhttp.onreadystatechange=function() {
+                if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                    document.getElementById("sanphamkhchon").innerHTML=xmlhttp.responseText; 
+                } 
+            }   
+            xmlhttp.open("GET", "../ajax/xoasp.php?idsp="+idsp, true);
+            xmlhttp.send();
         }
     </script>
 </body>
